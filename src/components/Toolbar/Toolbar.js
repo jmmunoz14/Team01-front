591fcb22dead02399fea369634e17993d9e84f2f
Juan <jm.munoz14@uniandes.edu.co>
2019-03-28 22:54:52 -0500

    Update Toolbar.js

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,36 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom"
 
-const toolbar = props => (
-    <nav className="navbar navbar-expand-sm navbar-dark navbar-custom p-0">
-        <Link to="/" className="navbar-brand">The Math Games!</Link>
-        <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarMenu">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/home">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/blogs">Blogs</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/partidas">Partidas</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/quiz">Preguntas</Link>
-                </li>
-            </ul>
-        </div>
-    </nav>
-);
+export class Toolbar extends Component {
 
     render() {
 
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-                <Link to="/" className="navbar-brand">MathG...</Link>
+            <nav className="navbar navbar-expand-sm navbar-dark navbar-custom p-0">
+        <Link to="/" className="navbar-brand">The Math Games!</Link>
                 <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
                     <span className="navbar-toggler-icon"></span>
                 </button>
