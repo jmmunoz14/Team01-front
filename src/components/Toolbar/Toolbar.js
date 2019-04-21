73ddb9257409b4e5057678160e9d56ff0fe8e640
Juan <jm.munoz14@uniandes.edu.co>
2019-04-21 00:32:46 -0500

    Update Toolbar.js

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -103,16 +103,14 @@ export class Toolbar extends Component {
         return (
 
 
-            <nav className="navbar navbar-expand-lg navbar-dark navbar-custom p-0">
+            <nav className="navbar navbar-expand-sm navbar-dark navbar-custom p-0">
                 <Link to="/" className="navbar-brand">
                     <FormattedMessage
                         id="Toolbar.title"
                         defaultMessage="The Math Games!"
                     />
                 </Link>
-                <li className="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-                    <span className="navbar-toggler-icon"></span>
-                </li>
+                
                 <div className="collapse navbar-collapse" id="navbarMenu">
                     <ul className="navbar-nav mr-auto">
                         <li>
